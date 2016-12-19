61b8c622cb2a188b12e04f784e452e995fb13b2a
hedong <dong.he01@billionsfinance.cn>
2016-12-19 18:00:16 +0800

    11

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -26,5 +26,50 @@ class IndexController extends Controller {
 	public function welcome(){
 		echo 'welcome';
 	}
+
+    public function manager(){
+        $r = M('wkmanager')->select();
+        $this->assign("list",$r);
+        $this->display();
+    }
+
+    public function addman(){
+        if(I('id') != ''){
+            $r = M('wkmanager')->where(array('id'=>I('id')))->find();
+            $this->assign("r",$r);
+        }
+        $this->display();
+    }
+
+    public function do_add(){
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize   =     3145728 ;// 设置附件上传大小
+        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath  =     __ROOT__.'/uploads/'; // 设置附件上传根目录
+        $upload->savePath  =     ''; // 设置附件上传（子）目录
+        // 上传文件 
+        $info   =   $upload->upload();
+        if($info['pic']){
+            $arr['pic'] = $info['pic']['savepath'].$info['pic']['savename'];
+        }
+        $arr['name'] = I('name');
+        $arr['zhiwei'] = I('zhiwei');
+        $arr['mobile'] = I('mobile');
+        $arr['wechat'] = I('wechat');
+        $arr['describe'] = I('describe');
+    
+        if(I('id') != ''){
+            //更新
+            if(M('wkmanager')->where(array('id'=>I('id')))->save($arr)){
+                $this->success('编辑成功');
+            }
+        }else{
+            //写入            
+            if(M('wkmanager')->add($arr)){
+                $this->success('插入成功');
+            }
+        }
+       
+    }
 	
 }
--- appnew/pages/xiangqing/xiangqing.js
+++ appnew/pages/xiangqing/xiangqing.js
@@ -5,20 +5,38 @@ var dataArr = null;
 
 Page({
     data : {
-        
+        hasLocation:null,
     },
 
-
+    getLocation: function(e){
+          console.log(666)
+          wx.getLocation({
+              type: 'wgs84',
+              success: function(res) {
+                console.log(res)
+                var latitude = res.latitude
+                var longitude = res.longitude
+                var speed = res.speed
+                var accuracy = res.accuracy
+                wx.openLocation({
+                    latitude: latitude,
+                    longitude: longitude,
+                    scale: 28
+                })
+              }
+          })
+      },
+    
     onLoad: function (options) {
         console.log('onLoad')
         console.log(this.data.motto)
         var that = this
         //调用应用实例的方法获取全局数据
         app.getUserInfo(function(userInfo){
-        //更新数据
-        that.setData({
-            userInfo:userInfo
-        })
+          //更新数据
+          that.setData({
+              userInfo:userInfo
+          })
         })
 
         that.setData({
@@ -27,10 +45,19 @@ Page({
         var id = this.data.id
         getDeatail(id,this)
         //console.log(r)
+
+        
+
+
+        
+
+        
   }
     
 
 })
+
+
 
 function getDeatail(id,that){
   wx.request({
