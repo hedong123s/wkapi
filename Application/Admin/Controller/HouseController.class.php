5d3cbfb95b3ee81d5f2b746c7a6a9dd5626972d7
hedong <hedong123s@163.com>
2017-01-06 21:56:37 +0800

    11

--- Application/Admin/Controller/HouseController.class.php
+++ Application/Admin/Controller/HouseController.class.php
@@ -31,8 +31,15 @@ class HouseController extends Controller {
 		$this->display();
 	}
 
+	public function delete(){
+		$id = I('id');
+		if(M("wk")->delete($id)){
+			$this->success("删除成功");
+		}
+	}
+
 	public function do_add(){
-		if(I('act' == 'edit')){
+		if(I('act') == 'edit'){
 			$data['id'] = I('id');
 			$data['title'] = I('title');
 			$data['area'] = I('area');
@@ -78,6 +85,50 @@ class HouseController extends Controller {
     			var_dump($re);
     		}
 
+		}else{
+			$data['title'] = I('title');
+			$data['area'] = I('area');
+			$data['price'] = I('price');
+			$data['price_type'] = I('price_type');
+			$data['keyword'] = I('keyword');
+			$data['huxin'] = I('huxin');
+			$data['huxin_type']= I('huxin_type');
+			$data['mianji'] = I('mianji');
+			$data['sale_status'] = I('sale_status');
+			$data['dizhi'] = I('dizhi');
+			$r = M('wk')->add($data);
+
+			$upload = new \Think\Upload();// 实例化上传类
+		    $upload->maxSize   =     3145728 ;// 设置附件上传大小
+		    $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+		    $upload->rootPath  =     __ROOT__.'/uploads/'; // 设置附件上传根目录
+		    $upload->savePath  =     ''; // 设置附件上传（子）目录
+		    // 上传文件 
+		    $info   =   $upload->upload();
+		    //var_dump($info);
+		    //var_dump($upload->getError());
+		    //var_dump($upload);
+		    //exit();
+
+	    	if($info['file-1']){
+	    		$arr['img1'] = $info['file-1']['savepath'].$info['file-1']['savename'];
+	    	}
+	    	if($info['file-2']){
+	    		$arr['img2'] = $info['file-2']['savepath'].$info['file-2']['savename'];
+	    	}
+	    	if($info['file-3']){
+	    		$arr['img3'] = $info['file-3']['savepath'].$info['file-3']['savename'];
+	    	}
+	    	if($info['file-4']){
+	    		$arr['img4'] = $info['file-4']['savepath'].$info['file-4']['savename'];
+	    	}
+	    	$arr['rid'] = $r;
+    		$re = M("wkdetail")->add($arr);
+    		if($re){
+    			$this->success('插入成功');
+    		}else{
+    			var_dump($re);
+    		}
 		}
 		
 	}
