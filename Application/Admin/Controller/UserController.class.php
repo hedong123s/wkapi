7da050c7f633d05ea4491b0bd846f17cbbfcc816
hedong <hedong123s@163.com>
2017-02-17 21:25:18 +0800

    11

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -33,5 +33,55 @@ class UserController extends BaseController {
 		$this->assign("res",$res);
 		$this->display();
 	}
+
+	/**
+	 * 导出
+	 */
+	public function export(){
+
+		Vendor('PHPExcel.PHPExcel');
+		$objExcel=new \PHPExcel();  //创建一个excel
+
+		
+		//设置导出内容
+		$objExcel->getActiveSheet()->setCellValue('A1', 'ID'); 
+		$objExcel->getActiveSheet()->setCellValue('B1', 'openid'); 
+		$objExcel->getActiveSheet()->setCellValue('C1', '性别');
+		$objExcel->getActiveSheet()->setCellValue('D1', '地区');
+		$objExcel->getActiveSheet()->setCellValue('E1', '价格');
+		$objExcel->getActiveSheet()->setCellValue('F1', '户型');
+		$objExcel->getActiveSheet()->setCellValue('G1', '浏览');
+		$objExcel->getActiveSheet()->setCellValue('H1','添加时间');
+
+		$res = M('wklog')->order("time desc")->select();
+		
+		$i=2;
+		foreach($res as $val){
+			$arr = explode('|', $val['select']);
+			$objExcel->getActiveSheet()->setCellValue('A'.$i, $val['id']); 
+			$objExcel->getActiveSheet()->setCellValue('B'.$i, $val['openid']); 
+			$objExcel->getActiveSheet()->setCellValue('C'.$i, $val['gender'] == 1 ? '男':'女');
+			$objExcel->getActiveSheet()->setCellValue('D'.$i, $arr[0]);
+			$objExcel->getActiveSheet()->setCellValue('E'.$i, $arr[1]);
+			$objExcel->getActiveSheet()->setCellValue('F'.$i, $arr[2]);
+			$objExcel->getActiveSheet()->setCellValue('G'.$i, $val['info']);
+			$objExcel->getActiveSheet()->setCellValue('H'.$i, date("Y-m-d H:i:s",$val['time']));
+			$i++;
+		}
+		
+		//设置导出文件名
+		$outputFileName = "浏览日志".date("Y-m-d").'.xls'; 
+		$xlsWriter = new \PHPExcel_Writer_Excel5($objExcel); 
+		header("Content-Type: application/force-download"); 
+		header("Content-Type: application/octet-stream"); 
+		header("Content-Type: application/download"); 
+		header('Content-Disposition:inline;filename="'.$outputFileName.'"'); 
+		header("Content-Transfer-Encoding: binary"); 
+		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
+		header("Pragma: no-cache"); 
+		$xlsWriter->save( "php://output" );
+	}
 	
 }
