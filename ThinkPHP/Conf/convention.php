e7d47c9f73eb43b2f17a32adea2b0475638f2a38
hedong <dong.he01@billionsfinance.cn>
2017-05-09 14:47:37 +0800

    11

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -50,10 +50,10 @@ return  array(
 
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '115.28.73.129', // 服务器地址
-    'DB_NAME'               =>  'hd_wordpress',          // 数据库名
+    'DB_HOST'               =>  '121.201.21.213', // 服务器地址
+    'DB_NAME'               =>  'appgzwkh',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'Gaodunming5',          // 密码
+    'DB_PWD'                =>  'Hed123456',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'hd_',    // 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
