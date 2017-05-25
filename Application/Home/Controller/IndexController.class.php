7657025351a89012d853773a70689d07135350be
hedong <dong.he01@billionsfinance.cn>
2017-05-25 15:14:02 +0800

    11

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -215,7 +215,7 @@ class IndexController extends Controller {
 	}
 
 	public function manager(){
-		$map['team_id'] = I("team");
+		$map['team_id'] = I("team") != 666 ? I("team") : '';
 		$res = M("wkmanager")->where($map)->order('rand()')->limit(3)->select();
 		if($res){
 			exit(json_encode(array('err'=>0,'msg'=>'查询成功','res'=>$res)));
